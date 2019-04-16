86248dcb1ee744e3531fb8f64e8dde419c36d110
bethm <bethm@172.16.42.4>
2019-04-16 14:43:21 -0700

    share project with respository- partial formatting completed

---
+++ tucsondogsitters/index.php
@@ -0,0 +1,24 @@
+<?php include("controller/include.php") ?>
+<!DOCTYPE HTML>
+<html>
+<head>
+ <!-- something better stage now! -->
+<meta name = "keywords" content="dog sitting, dogsitter, house sitting, dog walking">
+  <meta name="description" content="Dog and cat sitting in Tucson">
+  <meta name="author" content="Beth Marlatt">
+  <link rel="shortcut icon" href="images/favicon.ico">
+ 
+  <script src="controller/js.js"></script>
+  <title>Tucson Dogsitters</title>
+  <link rel="stylesheet" type="text/css" href="dogsitters.css" />
+</head>
+<body>
+<div class="container"><div class="header"><img src="images/headerImage.jpg" alt="header picture">
+<div class="navbar"></div>
+
+<?php include("controller/include.php") ?>
+</div>
+</div>
+</body>
+
+</html>
